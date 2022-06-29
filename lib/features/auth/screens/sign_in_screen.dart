1741243756de2e5f8fbc7ebb0a16c45a0852cfa8
Rindra <ranaivosoarindra3@gmail.com>
2022-06-29 11:49:42 +0300

    sign in ui done, keyboard fixed

--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -18,188 +18,202 @@ class _SignInScreenState extends State<SignInScreen> {
   bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 17),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Welcome
-                  Text(
-                    "Welcome Back",
-                    style: TextStyle(
-                      color: GlobalVariables.primaryColor,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold
-                    ),  
-                  ),
-
-                  SizedBox(
-                    height: 10,
-                  ),
-
-                  // top text
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        height:1.5,
-                        color: Color(0xFF524B6B),
-                        fontWeight: FontWeight.w400
-                      )
-                    ),
-                  ),
-                ],
-              ),
-              // Form
-              Column(
-                children: [
-                  ListTile(
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 5.0),
-                      child: Text(
-                        "Email",
-                        style: TextStyle(
-                          color: Color(0xFF0D0140),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    subtitle: Form(
-                      child: CustomFormField(withSuffixIcon: false,),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  ListTile(
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 5.0),
-                      child: Text(
-                        "Password",
-                        style: TextStyle(
-                          color: Color(0xFF0D0140),
-                          fontWeight: FontWeight.bold,
-                        ),  
-                      ),
-                    ),
-                    subtitle: Form(
-                      child: CustomFormField(
-                        withSuffixIcon: true,
-                        suffixIconHiden: Icon(Icons.visibility_off),
-                        suffixIconShown: Icon(Icons.visibility)
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 10),
-
-                  // form specific
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Transform.scale(
-                            scale: 1.50,
-                            child: Checkbox(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(4)),
-                                checkColor: GlobalVariables.secondaryColor,
-                                activeColor: Color(0xFFE6E1FF),
-                                value: isChecked,
-                                onChanged: (bool? val) {
-                                  setState(() {
-                                    isChecked = val!;
-                                  });
-                                }),
-                          ),
-                          Text(
-                            "Remember me",
-                            style: TextStyle(
-                              color: Color(0xFFAAA6B9),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 16,
-                            ),  
-                          ),
-                        ],
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Text(
-                          "Forgot Password ?",
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(), // set focus out when the user click anywhere else
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 17, vertical: 30),
+              child: Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Welcome
+                        Text(
+                          "Welcome Back",
                           style: TextStyle(
-                            color: Color(0xFF0D0140),
-                            fontWeight: FontWeight.w700,
-                            fontSize: 16,
+                            color: GlobalVariables.primaryColor,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
                           ),  
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-
-              Column(
-                children: [
-                  /// Buttons
-                  // login button
-                  GlobalButton(
-                    onTap: () {},
-                    text: "login",
-                    backgroundColor: GlobalVariables.primaryColor,
-                    textColor: Colors.white,
-                    withIcon: false,
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // google button
-                  GlobalButton(
-                    onTap: () {},
-                    text: "sign in with google",
-                    backgroundColor: GlobalVariables.tertiaryColor,
-                    textColor: GlobalVariables.primaryColor,
-                    withIcon: true,
-                    imageAsset: "assets/icons/google_icon.png",
-                  ),
-
-                  SizedBox(height: 15),
-
-                  // go to sign up
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "You don't have an account yet ?  ",
-                        style: TextStyle(
-                          color: Color(0xFF524B6B),
-                          fontWeight: FontWeight.w600,
+          
+                        SizedBox(
+                          height: 10,
                         ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(context, SignUpScreen.routeName);
-                        },
-                        child: Text(
-                          "Sign up",
-                          style: TextStyle(
-                            color: Color(0xFFFF9228),
-                            fontWeight: FontWeight.w600,
-                            decoration: TextDecoration.underline
+          
+                        // top text
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              height:1.5,
+                              color: Color(0xFF524B6B),
+                              fontWeight: FontWeight.w400
+                            )
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                      ],
+                    ),
+    
+                    SizedBox(
+                      height: 50,
+                    ),
+    
+                    // Form
+                    Column(
+                      children: [
+                        ListTile(
+                          title: Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: Text(
+                              "Email",
+                              style: TextStyle(
+                                color: Color(0xFF0D0140),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          subtitle: Form(
+                            child: CustomFormField(withSuffixIcon: false,),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        ListTile(
+                          title: Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: Text(
+                              "Password",
+                              style: TextStyle(
+                                color: Color(0xFF0D0140),
+                                fontWeight: FontWeight.bold,
+                              ),  
+                            ),
+                          ),
+                          subtitle: Form(
+                            child: CustomFormField(
+                              withSuffixIcon: true,
+                              suffixIconHiden: Icon(Icons.visibility_off),
+                              suffixIconShown: Icon(Icons.visibility)
+                            ),
+                          ),
+                        ),
+          
+                        SizedBox(height: 10),
+          
+                        // form specific
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Transform.scale(
+                                  scale: 1.50,
+                                  child: Checkbox(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(4)),
+                                      checkColor: GlobalVariables.secondaryColor,
+                                      activeColor: Color(0xFFE6E1FF),
+                                      value: isChecked,
+                                      onChanged: (bool? val) {
+                                        setState(() {
+                                          isChecked = val!;
+                                        });
+                                      }),
+                                ),
+                                Text(
+                                  "Remember me",
+                                  style: TextStyle(
+                                    color: Color(0xFFAAA6B9),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 15,
+                                  ),  
+                                ),
+                              ],
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Forgot Password ?",
+                                style: TextStyle(
+                                  color: Color(0xFF0D0140),
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 15,
+                                ),  
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+    
+                    SizedBox(height: 37,),
+          
+                    Column(
+                      children: [
+                        /// Buttons
+                        // login button
+                        GlobalButton(
+                          onTap: () {},
+                          text: "login",
+                          backgroundColor: GlobalVariables.primaryColor,
+                          textColor: Colors.white,
+                          withIcon: false,
+                        ),
+          
+                        SizedBox(height: 20),
+          
+                        // google button
+                        GlobalButton(
+                          onTap: () {},
+                          text: "sign in with google",
+                          backgroundColor: GlobalVariables.tertiaryColor,
+                          textColor: GlobalVariables.primaryColor,
+                          withIcon: true,
+                          imageAsset: "assets/icons/google_icon.png",
+                        ),
+          
+                        SizedBox(height: 15),
+          
+                        // go to sign up
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "You don't have an account yet ?  ",
+                              style: TextStyle(
+                                color: Color(0xFF524B6B),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(context, SignUpScreen.routeName);
+                              },
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(
+                                  color: Color(0xFFFF9228),
+                                  fontWeight: FontWeight.w600,
+                                  decoration: TextDecoration.underline
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ),
