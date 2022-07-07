0db725a0aa42a55bd5cbdd3e9e27ede9045c033e
Gérald Rindra RANAIVOSOA <rindra.ranaivosoa@paminnov.com>
2022-07-07 21:00:07 +0300

    error about provider fixed / fixed when login after sign up

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,18 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:job_finder/constants/global_variables.dart';
-import 'package:job_finder/features/auth/screens/check_email_screen.dart';
-import 'package:job_finder/features/auth/screens/forgot_password_screen.dart';
-import 'package:job_finder/features/auth/screens/sign_in_screen.dart';
-import 'package:job_finder/features/auth/screens/sign_up_screen.dart';
-import 'package:job_finder/features/auth/screens/successfully_reset_password_screen.dart';
-import 'package:job_finder/features/auth/services/auth_service.dart';
 import 'package:job_finder/features/splash/splashScreen.dart';
 import 'package:job_finder/providers/user.provider.dart';
 import 'package:job_finder/router.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MultiProvider(
-        providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
-        child: const MyApp()),
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider())
+      ],
+      child: MyApp(),
   ));
 }
 
@@ -30,13 +26,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  @override
-  void initState() {
-    super.initState();
-   // authService.getUserData(context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -57,10 +46,9 @@ class _MyAppState extends State<MyApp> {
               child: Text(
                 "Jobspot",
                 style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22
-                ),
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22),
               ),
             ),
           ],
